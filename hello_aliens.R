2a9ef2efdfab1541a9c49202f79775bcc9ccfebd
Cassie Peace <cassiepeace@Cassies-MacBook-Pro-4.local>
2017-12-08 18:55:43 -0500

    alien message

---
+++ hello_aliens.R
@@ -0,0 +1,5 @@
+cat("Hello. This message is reaching you from the planet Earth. 
+    We are a group of scientists and explorers, searching for other extraterrestrial lifeforms. 
+    We are excited to have made contact with you, and we are hoping to learn more about you in the pursuit of knowledge. 
+    Please respond back if you wish to enter into futher communication with us. 
+    We come in peace.")
